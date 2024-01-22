ac41f13039cf96a9e6d38b37d99dcce95fb5d544
cocktailpeanut <cocktailpeanuts@proton.me>
2024-01-22 10:59:24 -0500

    init

---
+++ install.js
@@ -0,0 +1,37 @@
+module.exports = {
+  "cmds": {
+    "nvidia": "pip install torch torchvision torchaudio xformers --index-url https://download.pytorch.org/whl/cu118",
+    "amd": "pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm5.6",
+    "default": "pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cpu"
+  },
+  "requires": [{
+    "type": "conda",
+    "name": "ffmpeg",
+    "args": "-c conda-forge"
+  }],
+  "run": [{
+    "method": "shell.run",
+    "params": {
+      //"message": "git clone https://github.com/candywrap/audiocraft app"
+      "message": [
+        "git lfs install",
+        "git clone https://huggingface.co/spaces/cocktailpeanut/InstantID app"
+      ]
+    }
+  }, {
+    "method": "shell.run",
+    "params": {
+      "venv": "env",
+      "path": "app",
+      "message": [
+        "{{(gpu === 'nvidia' ? self.cmds.nvidia : (gpu === 'amd' ? self.cmds.amd : self.cmds.default))}}",
+        "pip install -r requirements.txt",
+      ]
+    }
+  }, {
+    "method": "notify",
+    "params": {
+      "html": "Click the 'start' tab to get started!"
+    }
+  }]
+}
