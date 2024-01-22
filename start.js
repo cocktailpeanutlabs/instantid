7b559f37966d98858e0a2a8b572f77cc8564a4d8
cocktailpeanut <cocktailpeanuts@proton.me>
2024-01-22 18:42:07 -0500

    update

--- start.js
+++ start.js
@@ -5,9 +5,6 @@ module.exports = {
     "params": {
       "path": "app",
       "venv": "env",
-      "env": {
-        "PYTORCH_MPS_HIGH_WATERMARK_RATIO": "0.0"
-      },
       "message": "python app.py",
       "on": [{ "event": "/http:\/\/[0-9.:]+/", "done": true }]
     }
