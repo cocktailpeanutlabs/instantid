af1602aba404b318bd1e45b36973c2f1997f68f7
6Morpheus6 <wanton-decay@gmx.net>
2024-10-28 06:44:02 +0100

    huggingface_hub fix

--- install.js
+++ install.js
@@ -1,36 +1,41 @@
 module.exports = {
-  "cmds": {
-    "nvidia": "pip install torch torchvision torchaudio xformers --index-url https://download.pytorch.org/whl/cu118",
-    "amd": "pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm5.6",
-    "default": "pip install torch torchvision torchaudio"
-  },
-  "requires": [{
-    "type": "conda",
-    "name": "ffmpeg",
-    "args": "-c conda-forge"
-  }],
   "run": [{
-    "method": "shell.run",
-    "params": {
-      "message": [
+    method: "shell.run",
+    params: {
+      message: [
         "git lfs install",
         "git clone https://huggingface.co/spaces/cocktailpeanut/InstantID2 app"
       ]
     }
   }, {
-    "method": "shell.run",
-    "params": {
-      "venv": "env",
-      "path": "app",
-      "message": [
-        "{{(gpu === 'nvidia' ? self.cmds.nvidia : (gpu === 'amd' ? self.cmds.amd : self.cmds.default))}}",
+    method: "script.start",
+    params: {
+      uri: "torch.js",
+      params: {
+        venv: "env",
+        path: "app",
+        xformers: true
+      }
+    }
+  }, {
+    method: "shell.run",
+    params: {
+      venv: "env",
+      path: "app",
+      message: [
+        "pip install huggingface_hub==0.25.2",
         "pip install -r requirements.txt",
       ]
     }
   }, {
-    "method": "notify",
-    "params": {
-      "html": "Click the 'start' tab to get started!"
+    method: "fs.link",
+    params: {
+      venv: "app/env"
+    }
+  }, {
+    method: "notify",
+    params: {
+      html: "Click the 'start' tab to get started!"
     }
   }]
 }
